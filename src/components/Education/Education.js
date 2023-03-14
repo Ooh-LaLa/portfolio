98a440ac9cc106255495dd2bde7149bbc604a0c0
ooh-lala <laurentozzi.code@gmail.com>
2023-03-14 18:01:53 -0400

    change file path of booksPhoto

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import EducationImg from "/Users/lauren/code/personal/portfolio/killer-portfolio/Portfolio/src/components/asset/booksPhoto.jpg";
+import EducationImg from 'public/booksPhoto.jpg';
 // import EducationData from "../../Data/EducationData";
 import Certifications from "../Certifications/Certifications";
 import Degree from "./Degree";
