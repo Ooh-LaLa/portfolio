5afc87b6152cd0420d6b0cdb08c3894a40f03e58
ooh-lala <laurentozzi.code@gmail.com>
2023-03-14 20:46:20 -0400

    extend footer on homepage

--- src/Data/EducationData.js
+++ src/Data/EducationData.js
@@ -1,13 +1,23 @@
 
 
 const EducationData=
-    {collegeName:"General Assembly",
+    {
+        collegeName:"General Assembly",
     courseName:"Software Engineering Immersive",
     couseStartYear:"2022",
     courseEndYear:"2023",
     details:[
         'Completed a full-stack software engineering immersive program involving an intensive 24-week, 450+ hour program focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling and team-collaboration strategies. Developed a portfolio of individual and group projects.'],
-}
+    }
+//     {
+//     collegeName:"MassBay Community College",
+//     courseName:"Science - General Studies",
+//     couseStartYear:"2014",
+//     courseEndYear:"2017",
+//     details:[
+//         'Associate's Degree'],
+//     },
+// ]
 
 
 export default EducationData;
