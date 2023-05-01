6dfcc67e80ba3db841f0796e0f07c51f1e6c8d08
ooh-lala <laurentozzi.code@gmail.com>
2023-05-01 17:30:28 -0400

    change home route

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App=()=> {
                     <CSSTransition timeout={300} classNames='fade' key={location.key}>
                         <Switch location={location}>
                             <Route path="/" exact>
-                                <Redirect to="/home" />
+                                <Home />
                             </Route>
                             <Route path="/home">
                                 <Home />
